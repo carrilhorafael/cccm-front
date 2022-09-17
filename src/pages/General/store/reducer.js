e71b3eb42356dc1dc14db187a51e93d9379e6c64
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2022-09-17 17:50:25 -0300

    Remove unused ActionTypes to General reducer

--- src/pages/General/store/reducer.js
+++ src/pages/General/store/reducer.js
@@ -10,50 +10,6 @@ function reducer(state, action) {
         ...action.payload
       }
 
-    case ActionType.ADD_TO_CHURCHES:
-      return {
-        ...state,
-        churches: [...state.churches, action.payload]
-      }
-
-    case ActionType.UPDATE_CHURCH: {
-      const actualIndex = state.churches.findIndex(({ id }) => id === action.payload.id)
-
-      return {
-        ...state,
-        churches: [
-          ...state.churches.slice(0, actualIndex - 1),
-          action.payload.data,
-          ...state.churches.slice(actualIndex + 1)
-        ]
-      }
-    }
-
-    case ActionType.REMOVE_CHURCH:
-      return {
-        ...state,
-        churches: [...state.churches.filter(({ id }) => id !== action.payload)]
-      }
-
-
-
-    case ActionType.SET_FETCHING:
-      return {
-        ...state,
-        status: ActionStatus.FETCHING
-      }
-
-    case ActionType.UPDATE_COMPLETE:
-      return {
-        ...state,
-        status: ActionStatus.COMPLETED
-      }
-
-    case ActionType.CREATE:
-      return {
-        ...state
-      }
-
     default:
       throw new Error(`Unhandled action type ${action.type}`)
   }
